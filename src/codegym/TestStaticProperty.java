63cd0b6b170b0cdd7e9458ada480d70c1e35e2a8
dhviet12 <73893345+dhviet12@users.noreply.github.com>
2020-12-15 15:59:41 +0700

    first commit

---
+++ src/codegym/TestStaticProperty.java
@@ -0,0 +1,13 @@
+package codegym;
+
+public class TestStaticProperty {
+    public static void main(String[] args) {
+        Car car1 = new Car("Mazda3","Skyactiv 3");
+        System.out.println(Car.numberOfCars);
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+        System.out.println(Car.numberOfCars);
+        Car car3= new Car("Mazda CX5","Skyactiv 5");
+        System.out.println(Car.numberOfCars);
+
+    }
+}
